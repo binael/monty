3c4640e4cdbd1794b6c362eb663841e299298c33
binael <99293451+binael@users.noreply.github.com>
2022-06-17 02:27:14 +0100

    Update arithematic.c

--- arithematic.c
+++ arithematic.c
@@ -32,3 +32,130 @@ void add(stack_t **stack, unsigned int line_number)
 	free(dlist);
 }
 
+/**
+ * add - adds the top two nodes of stack
+ * @stack: stack
+ * @line_number: unsigned int line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *dlist = *stack;
+	int sum;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		deallocate(stack);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (dlist->next->next)
+		dlist = dlist->next;
+	sum = dlist->n + dlist->next->n;
+	pop(stack, line_number);
+	pop(stack, line_number);
+
+	if (!stack || !(*stack))
+		add_dnodeint(stack, sum);
+	else
+		add_nodeint_end(stack, sum);
+	free(dlist);
+}
+
+/**
+ * add - adds the top two nodes of stack
+ * @stack: stack
+ * @line_number: unsigned int line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *dlist = *stack;
+	int sum;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		deallocate(stack);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (dlist->next->next)
+		dlist = dlist->next;
+	sum = dlist->n + dlist->next->n;
+	pop(stack, line_number);
+	pop(stack, line_number);
+
+	if (!stack || !(*stack))
+		add_dnodeint(stack, sum);
+	else
+		add_nodeint_end(stack, sum);
+	free(dlist);
+}
+
+/**
+ * add - adds the top two nodes of stack
+ * @stack: stack
+ * @line_number: unsigned int line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *dlist = *stack;
+	int sum;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		deallocate(stack);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (dlist->next->next)
+		dlist = dlist->next;
+	sum = dlist->n + dlist->next->n;
+	pop(stack, line_number);
+	pop(stack, line_number);
+
+	if (!stack || !(*stack))
+		add_dnodeint(stack, sum);
+	else
+		add_nodeint_end(stack, sum);
+	free(dlist);
+}
+
+/**
+ * add - adds the top two nodes of stack
+ * @stack: stack
+ * @line_number: unsigned int line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *dlist = *stack;
+	int sum;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		deallocate(stack);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (dlist->next->next)
+		dlist = dlist->next;
+	sum = dlist->n + dlist->next->n;
+	pop(stack, line_number);
+	pop(stack, line_number);
+
+	if (!stack || !(*stack))
+		add_dnodeint(stack, sum);
+	else
+		add_nodeint_end(stack, sum);
+	free(dlist);
+}

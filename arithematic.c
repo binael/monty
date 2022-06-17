d07d1f4833216f2e5995b49c7114384ece0bb722
binael <nbinael@yahoo.com>
2022-06-16 18:19:55 -0700

    more bug fixing

--- arithematic.c
+++ arithematic.c
@@ -15,7 +15,7 @@ void add(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack) || !((*stack)->next))
 	{
 		deallocate(stack);
-		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
